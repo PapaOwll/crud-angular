9e4a38d6203a52e0493040ee253148ce6d117acd
PapaOwL <mahdiabdolmaleki96@gmail.com>
2023-02-25 00:03:13 +0330

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {EmpAddEditComponent} from "./emp-add-edit/emp-add-edit.component";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'crud-app';
+  constructor(private dialog:MatDialog) {}
+  openAddEditForm(){
+    this.dialog.open(EmpAddEditComponent)
+  }
+}
+
